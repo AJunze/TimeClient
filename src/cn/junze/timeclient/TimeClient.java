40a1dc3208a3ccfd039c7beb45f0b35fa33a3507
Junze <827835068@qq.com>
2018-05-13 16:40:36 +0800

    test

--- src/cn/junze/timeclient/TimeClient.java
+++ src/cn/junze/timeclient/TimeClient.java
@@ -8,13 +8,14 @@ public class TimeClient {
 
         int port = 18080;
 
+
         if(args != null && args.length > 0){
 
 
             port = Integer.valueOf(args[0]);
         }
 
-        new Thread(new TimeClientHandle("127.0.0.1",port),"client01").start();
+        new Thread(new TimeClientHandle("127.0.0.1",port),"client001").start();
 
     }
 }
